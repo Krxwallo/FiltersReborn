136aa6b8e1bc4b103c71f88a86b842dce5962669
Krxwallo <a@b.c>
2020-08-07 21:35:58 +0200

    Version Update 1.0.1

--- src/main/java/com/mrcrayfish/filters/Reference.java
+++ src/main/java/com/mrcrayfish/filters/Reference.java
@@ -7,6 +7,6 @@ public class Reference
 {
     public static final String MOD_ID = "filters";
     public static final String NAME = "Filters Reborn";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String AUTHOR = "justAm0dd3r & MrCrayfish";
 }
